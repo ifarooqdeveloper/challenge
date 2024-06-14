9206b457a2cb4fd723e9eff970d38ac3a30a49ad
Iram Farooq <ifarooqdeveloper@gmail.com>
2024-06-14 20:30:53 +0500

    Completed test task

---
+++ server/core/urls.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Test Task API's",
+      default_version='v1.0',
+      description="This is the API documentation for test task",
+      # terms_of_service="https://www.google.com/policies/terms/",
+      # contact=openapi.Contact(email="contact@snippets.local"),
+      # license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+        # Swagger UI paths
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path('admin/', admin.site.urls),
+    path('auth/', include('user.urls', namespace='user')),
+    path('transaction/', include('transaction.urls', namespace='transaction')),
+]
